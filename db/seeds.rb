85f9990312c078972a23cf5d1562311f85b9b2c6
Sebastian Burke <burke.sebastian1@gmail.com>
2024-09-15 16:30:42 +0700

    Update seeds.rb for Cloudinary compatibility

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,12 +20,25 @@ ActiveRecord::Base.transaction do
       params["images"].each do |image_file|
         image_path = Rails.root.join('app/assets/images', image_file)
         if File.exist?(image_path)
+          # Open the file and create a tempfile
+          file = File.open(image_path)
+          tempfile = Tempfile.new([ File.basename(image_path, ".*"), File.extname(image_path) ])
+          tempfile.binmode
+          tempfile.write(file.read)
+          tempfile.rewind
+
+          # Attach the image using the tempfile
           product.images.attach(
-            io: File.open(image_path),
+            io: tempfile,
             filename: File.basename(image_path),
             content_type: 'image/png'
           )
+
           puts "Image attached to #{product.name}: #{image_file}"
+
+          # Close and delete the tempfile
+          tempfile.close
+          tempfile.unlink
         else
           puts "Image not found: #{image_path}"
         end
