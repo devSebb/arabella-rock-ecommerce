911b53e6bb2f946d3d4975aad98401a96d178534
Sebastian Burke <burke.sebastian1@gmail.com>
2024-09-15 16:39:20 +0700

    Update seed file and view for Cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require "json"
 require "open-uri"
+require "cloudinary"
 
 filepath = Rails.root.join('db/products.json')
 serialized_products = File.read(filepath)
@@ -20,25 +21,19 @@ ActiveRecord::Base.transaction do
       params["images"].each do |image_file|
         image_path = Rails.root.join('app/assets/images', image_file)
         if File.exist?(image_path)
-          # Open the file and create a tempfile
-          file = File.open(image_path)
-          tempfile = Tempfile.new([ File.basename(image_path, ".*"), File.extname(image_path) ])
-          tempfile.binmode
-          tempfile.write(file.read)
-          tempfile.rewind
+          # Upload to Cloudinary
+          cloudinary_upload = Cloudinary::Uploader.upload(image_path)
 
-          # Attach the image using the tempfile
+          # Attach the image using the Cloudinary public_id
           product.images.attach(
-            io: tempfile,
+            io: StringIO.new(cloudinary_upload['secure_url']),
             filename: File.basename(image_path),
-            content_type: 'image/png'
+            content_type: 'image/png',
+            identify: false,
+            metadata: { public_id: cloudinary_upload['public_id'] }
           )
 
-          puts "Image attached to #{product.name}: #{image_file}"
-
-          # Close and delete the tempfile
-          tempfile.close
-          tempfile.unlink
+          puts "Image attached to #{product.name}: #{image_file} (Cloudinary public_id: #{cloudinary_upload['public_id']})"
         else
           puts "Image not found: #{image_path}"
         end
